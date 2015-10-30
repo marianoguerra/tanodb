a18de393b76a2e64da600c26706b4f01e01152bc
Mariano Guerra <mariano@marianoguerra.org>
2015-10-30 13:25:30 +0100

    add rcs_cowboy handlers and iorioui ui for rcs_cowboy

--- apps/tanodb/src/tanodb_app.erl
+++ apps/tanodb/src/tanodb_app.erl
@@ -16,6 +16,7 @@ start(_StartType, _StartArgs) ->
         {ok, Pid} ->
             ok = riak_core:register([{vnode_module, tanodb_vnode}]),
             ok = riak_core_node_watcher:service_up(tanodb, self()),
+            setup_basic_users_and_groups(),
 
             {ok, Pid};
         {error, Reason} ->
@@ -27,10 +28,53 @@ stop(_State) ->
 
 % private functions
 
+rcs_is_user_authorized(Req, #{user_ctx := _UserCtx}) ->
+    ResultJson = [{token, <<"atoken">>}],
+    ResultJsonBin = tanodb_json:encode(ResultJson),
+    Req1 = cowboy_req:set_resp_body(ResultJsonBin, Req),
+    {true, Req1}.
+
+add_user(Username, Password, Groups) ->
+    R = case riak_core_security:add_user(Username, [{"password", Password},
+                                                    {"groups", [Groups]}]) of
+            ok ->
+                R0 = riak_core_security:add_source([Username],
+                                                   {{127, 0, 0, 1}, 32},
+                                                   password, []),
+                lager:info("Add Source for ~p: ~p", [Username, R0]),
+                ok;
+            Other -> Other
+        end,
+    lager:info("Create user ~p: ~p", [Username, R]).
+
+add_group(Name, Groups) ->
+    R = riak_core_security:add_group(Name, Groups),
+    lager:info("Create Group ~p: ~p", [Name, R]).
+
+setup_basic_users_and_groups() ->
+    add_group(<<"users">>, []),
+    add_group(<<"admins">>, [{"groups", [<<"users">>]}]),
+
+    add_user(<<"user">>, "secret", <<"users">>),
+    add_user(<<"admin">>, "secret", <<"admins">>).
+
 routes() ->
+    JsonEncoder = fun tanodb_json:encode/1,
+    JsonDecoder = fun tanodb_json:decode/1,
+    IsAuthorizedFun = fun (Req, _Info) -> {true, Req} end,
+    IsUserAuthorizedFun = fun rcs_is_user_authorized/2,
+    RcsOpts = #{env_keys => [tanodb],
+                json_encoder => JsonEncoder, json_decoder => JsonDecoder,
+                is_user_authorized => IsUserAuthorizedFun,
+                base_uri => "/admin", is_authorized => IsAuthorizedFun},
+
     [
      {"/ping", tanodb_http_ping, []},
-     {"/metrics", tanodb_http_metrics, []}
+     {"/metrics", tanodb_http_metrics, []},
+     {"/admin/:action", rcs_cowboy_handler, RcsOpts},
+     {"/admin/:action/:param1", rcs_cowboy_handler, RcsOpts},
+     {"/ui/[...]", cowboy_static,
+      {priv_dir, tanodb, "ui", [{mimetypes, cow_mimetypes, all}]}}
     ].
 
 init_http() ->
